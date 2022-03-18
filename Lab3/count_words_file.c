dba87c5358414fc893cfcea8b4226b75d2addced
KmXK <guydo.k@yandex.by>
2022-03-18 11:56:10 +0300

    Исправлена опечатка в файле count_words_file.c

--- Lab3/count_words_file.c
+++ Lab3/count_words_file.c
@@ -15,9 +15,9 @@ int main(int argc, char **argv){
     for (int i = 1; i < argc; i++)
     {
         if(argv[i][0] == '-'){
-            for (int j = 1; j < sirlen(argv[i]); j++)
+            for (int j = 1; j < strlen(argv[i]); j++)
             {
-                switch (argv[i][j   ])
+                switch (argv[i][j])
                 {
                 case 'h':{
                     printf("Command format: «%s filepath word»\n", argv[0]);
